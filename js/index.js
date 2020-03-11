88e94face9422fbc408b4f82ed31eaa66901f9d2
Elijah Atkins <elijahorama@gmail.com>
2020-03-11 14:49:28 -0600

    added h4 and p content

--- js/index.js
+++ js/index.js
@@ -38,21 +38,52 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-  let logo = document.getElementById("logo-img");
-  logo.setAttribute('src', siteContent["nav"]["img-src"]);
+let logo = document.getElementById("logo-img");
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
-  let headerImg = document.getElementById("cta-img");
-  headerImg.setAttribute('src', siteContent["cta"]["img-src"])
 
-  let midPageAccent = document.getElementById("middle-img");
-  midPageAccent.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+// Update the img src for header Image and midPageAccent
+let headerImg = document.getElementById("cta-img");
+headerImg.setAttribute('src', siteContent["cta"]["img-src"])
 
-  const nav = document.getElementsByTagName("nav")
-  const link = document.getElementsByTagName("a")
-  link[0].textContent = siteContent["nav"]["nav-item-1"]
-  link[1].textContent = siteContent["nav"]["nav-item-2"]
-  link[2].textContent = siteContent["nav"]["nav-item-3"]
-  link[3].textContent = siteContent["nav"]["nav-item-4"]
-  link[4].textContent = siteContent["nav"]["nav-item-5"]
-  link[5].textContent = siteContent["nav"]["nav-item-6"]
+let midPageAccent = document.getElementById("middle-img");
+midPageAccent.setAttribute('src', siteContent["main-content"]["middle-img-src"])
 
+
+//Navbar links
+const nav = document.getElementsByTagName("nav")
+const link = document.getElementsByTagName("a")
+link[0].textContent = siteContent["nav"]["nav-item-1"]
+link[1].textContent = siteContent["nav"]["nav-item-2"]
+link[2].textContent = siteContent["nav"]["nav-item-3"]
+link[3].textContent = siteContent["nav"]["nav-item-4"]
+link[4].textContent = siteContent["nav"]["nav-item-5"]
+link[5].textContent = siteContent["nav"]["nav-item-6"]
+
+// cta content
+const h1 = document.getElementsByTagName("h1")
+const button = document.getElementsByTagName("button")
+button[0].textContent = siteContent["cta"]["button"]
+h1[0].textContent = siteContent["cta"]["h1"]
+
+//main-content
+const mainContent = document.querySelector('.top-content')
+const h4 = mainContent.getElementsByTagName("h4")
+const p = mainContent.getElementsByTagName('p')
+
+h4[0].textContent = siteContent["main-content"]["features-h4"]
+p[0].textContent = siteContent["main-content"]["features-content"]
+h4[1].textContent = siteContent["main-content"]["about-h4"]
+p[1].textContent = siteContent["main-content"]["about-content"]
+
+//bottom content
+const bottomContent = document.querySelector('.bottom-content');
+const bcH4 = bottomContent.getElementsByTagName("h4");
+const bcP = bottomContent.getElementsByTagName('p');
+
+bcH4[0].textContent = siteContent["main-content"]["services-h4"]
+bcP[0].textContent = siteContent["main-content"]["services-content"]
+bcH4[1].textContent = siteContent["main-content"]["product-h4"]
+bcP[1].textContent = siteContent["main-content"]["product-content"]
+bcH4[2].textContent = siteContent["main-content"]["vision-h4"]
+bcP[2].textContent = siteContent["main-content"]["vision-content"]
